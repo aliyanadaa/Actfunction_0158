aa1c66dda6e2cf695fea17cb12bcf6ed00e69329
aliyanadaa <aliyanadaq@gmail.com>
2026-03-02 17:24:21 +0700

    menambahkan library

--- luaslingkaran.cpp
+++ luaslingkaran.cpp
@@ -5,15 +5,15 @@ float phi = 3.14159;
 int r;
 
 void input (){
-    cout << "Masukkan jari jari:";
+    cout << "Masukkan jari jari : ";
     cin >> r;
 }
-float luas lingkaran (int b){
+float luas lingkaran (int b) {
     return phi*b*b;
 }
 
 void output (){
-    cout << "hasilnya=" << luaslingkaran (r);
+    cout << "hasilnya =" << luaslingkaran (r);
 }
 
 int main (){
